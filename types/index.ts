62621613791ab11e64cdab71a62c09c414661786
brboakye <73028728+brboakye@users.noreply.github.com>
2023-03-23 13:30:13 +0000

    ver4

--- types/index.ts
+++ types/index.ts
@@ -11,11 +11,11 @@ export enum OpenAIModelID {
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   [OpenAIModelID.GPT_3_5]: {
     id: OpenAIModelID.GPT_3_5,
-    name: "Default (GPT-3.5)"
+    name: "(GPT-3.5)"
   },
   [OpenAIModelID.GPT_4]: {
     id: OpenAIModelID.GPT_4,
-    name: "GPT-4"
+    name: "Default (GPT-4)"
   }
 };
 
